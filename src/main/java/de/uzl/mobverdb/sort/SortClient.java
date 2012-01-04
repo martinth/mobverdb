73e31f0f3b7a2a30bc8d45b238d8f2569c3af239
Martin Thurau <martin.thurau@gmail.com>
2012-01-04 11:52:44 +0100

    Implemented sorting on the client side as Thread and documented code.

--- src/main/java/de/uzl/mobverdb/sort/SortClient.java
+++ src/main/java/de/uzl/mobverdb/sort/SortClient.java
@@ -2,11 +2,17 @@ package de.uzl.mobverdb.sort;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import com.google.common.collect.Lists;
 
 import de.uzl.mobverdb.sort.base.ISortClient;
 
@@ -14,25 +20,59 @@ public class SortClient implements ISortClient, Serializable {
 	
 	/** generated */
     private static final long serialVersionUID = -4469350325989245038L;
-    private Iterator<String> iter;
-	private List<String> sorted;
+    
+	private List<String> toBeSorted;
+	private Iterator<String> iter;
+	
+    private FutureTask<Iterator<String>> sorterTask;
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void putWork(List<String> data) throws RemoteException {
-		sorted = new ArrayList<String>(data);
-		Collections.sort(sorted);
-		this.iter = sorted.iterator();
+		toBeSorted = Lists.newArrayList(data);
+		
+		/* the actual sorting will be done in a different thread so that this
+		 * method can return as fast as possible */
+		sorterTask = new FutureTask<Iterator<String>>(new Callable<Iterator<String>>() {
+            @Override
+            public Iterator<String> call() throws Exception {
+                Collections.sort(toBeSorted);
+                return toBeSorted.iterator();
+            }
+        });
+		executor.execute(sorterTask);
+		
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	public List<String> getMore(int blockSize) throws RemoteException {
 		LinkedList<String> ret = new LinkedList<String>();
-		for(int i = 0; i < blockSize; i++) {
-			if(iter.hasNext()) {
-				ret.add(iter.next());
-			} else {
-				break;
-			}
-		}
-		return ret;
+		
+		try {
+		    /* the call to get() will block until the sorting is done */
+            iter = sorterTask.get();
+            
+            /* Get as many elements as requested from the result list by using the 
+             * iterator. If the end of the list is reached we return less than requested
+             * so the requester can know there is no more data */ 
+            for(int i = 0; i < blockSize; i++) {
+            	if(iter.hasNext()) {
+            		ret.add(iter.next());
+            	} else {
+            		break;
+            	}
+            }
+            return ret;
+        } catch (InterruptedException e) {
+            throw new RemoteException("Sorting was interrupted.", e);
+        } catch (ExecutionException e) {
+            throw new RemoteException("An exception occured in the sorting task.", e);
+        }
 	}
 
 }
--- src/main/java/de/uzl/mobverdb/sort/base/ISortClient.java
+++ src/main/java/de/uzl/mobverdb/sort/base/ISortClient.java
@@ -11,8 +11,24 @@ import java.util.List;
  */
 public interface ISortClient extends Remote {
 	
+    /**
+     * Puts data to be sorted into the client.
+     * @param data data to be sorted
+     * @throws RemoteException
+     */
 	public void putWork(List<String> data) throws RemoteException;
 	
+	/**
+	 * Request a given amount of sorted data from this client. The request will forward 
+	 * the internal iterator. So two consecutive with blockSize 2 on the data [2,3,4,5]
+	 * will return [2,3] and [4,5]. If there is less data available than requested, the
+	 * returned list will be shorter than blockSize. This way the caller can know if 
+	 * their is more data to fetch.
+	 * @param blockSize the desired size of the to-be-returned list 
+	 * @return a list with the (maximal) length of blockSize. The returned list may be
+	 * shorter (or empty) if their is less data available than requested 
+	 * @throws RemoteException
+	 */
 	public List<String> getMore(int blockSize) throws RemoteException;
 
 }
